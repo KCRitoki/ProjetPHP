b2a9fa994f5c6e6f5157ce374b2e291727a151f3
Tom Duclos <48413394+Ritoki13@users.noreply.github.com>
2021-01-27 18:57:44 +0100

    Implémentation du CSS

--- VANESTARRE/main.php
+++ VANESTARRE/main.php
@@ -3,13 +3,28 @@
 	session_start();
 	
 	start_page('S\'inscrire');
-	echo '<form action="data-processing.php" method="post">
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Vanéstarre</title>
+		<link rel="stylesheet" href="./css/style.css">
+	</head>
+	<style>A {text-decoration: none;} </style>
+	<body>
+		<form action="data-processing.php" method="post" class="login">
 			E-mail<br/><input type="text" name="email"/><br/>
 			Identifiant<br/><input type="text" name="identifiant"/><br/>
 			Mot de passe<br/><input type="password" name="motdepasse"/><br/>
 			Vérification du mot de passe<br/><input type="password" name="motdepasse2"/><br/>
 			<input type="submit" name="action" value="mailer"/><br/>
-		  </form>';
+		</form>
+	</body>
+</html>
+
+<?php
 	if($_SESSION['error']=='erreurPassword')
 	{
 		echo $_SESSION['error'] . PHP_EOL;
