3f9790d4ecf5124a50a81d5ece81dbe533da847c
Quentin <quentin.roubin@etu.univ-amu.fr>
2021-01-28 19:12:04 +0100

    le post de message inscrit aussi le seuil love dans la base

--- VANESTARRE/Messages&Reactions/post-message.php
+++ VANESTARRE/Messages&Reactions/post-message.php
@@ -9,11 +9,42 @@
     //récupération du message
     $message = $_POST['message'];
 
+	//récupération des tags
+    $taglist = $_POST['tags'];
+	
     // Connexion à la base de données
     connect_bd($dbLink);
 
     // Insertion du message
-    $query = 'INSERT INTO messages (message) VALUES (\'' . $message . '\')';
+    $query = 'INSERT INTO messages (message, seuil_love) VALUES (\'' . $message . '\', \'' . rand(5,10) . '\');';
+
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+	
+	// Récupération de l'id du message
+    $query = 'SELECT * FROM messages WHERE id >= ALL (SELECT id FROM messages)';
+
+    if(!($dbResult = mysqli_query($dbLink, $query)))
+    {
+        echo 'Erreur dans requête<br />';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
+	$row=mysqli_fetch_assoc($dbResult);
+	$idMessage=$row['id'];
+	
+	// Insertion des tags
+    $query = 'INSERT INTO tags (taglist, idMessage) VALUES (\'' . $taglist . '\', \'' . $idMessage . '\')';
 
     if(!($dbResult = mysqli_query($dbLink, $query)))
     {
