34e07f1b3c5b838d17fd6880d1a66e28fe174cae
Quentin <quentin.roubin@etu.univ-amu.fr>
2021-01-28 19:26:53 +0100

    l'ajout d'un message sans tags ne crée plus de ligne dans la table tags

--- VANESTARRE/Messages&Reactions/post-message.php
+++ VANESTARRE/Messages&Reactions/post-message.php
@@ -44,17 +44,21 @@
 	$idMessage=$row['id'];
 	
 	// Insertion des tags
-    $query = 'INSERT INTO tags (taglist, idMessage) VALUES (\'' . $taglist . '\', \'' . $idMessage . '\')';
+	if(!empty($taglist))
+	{
+		$query = 'INSERT INTO tags (taglist, idMessage) VALUES (\'' . $taglist . '\', \'' . $idMessage . '\')';
 
-    if(!($dbResult = mysqli_query($dbLink, $query)))
-    {
-        echo 'Erreur dans requête<br />';
-        // Affiche le type d'erreur.
-        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-        // Affiche la requête envoyée.
-        echo 'Requête : ' . $query . '<br/>';
-        exit();
-    }
+		if(!($dbResult = mysqli_query($dbLink, $query)))
+		{
+			echo 'Erreur dans requête<br />';
+			// Affiche le type d'erreur.
+			echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+			// Affiche la requête envoyée.
+			echo 'Requête : ' . $query . '<br/>';
+			exit();
+		}
+	}
+    
 
     // Redirection
     header('Location: ../connected-vanestarre.php');
